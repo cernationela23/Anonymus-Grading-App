d93b8ca3e342e81eb70cd5ee797b35f33fa1efa7
iolanda08 <iolanda08caliman@gmail.com>
2025-12-05 21:59:51 +0200

    Connect to postgres database

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const sequelize = require('./config/sequelize'); 
+
 const app = express();
+
+sequelize.authenticate()
+  .then(() => console.log('DB conectata!'))
+  .catch(err => console.error('Eroare DB:', err.message));
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Serverul functioneaza corect');
+  res.send('Server + DB functioneaza');
 });
 
 module.exports = app;
