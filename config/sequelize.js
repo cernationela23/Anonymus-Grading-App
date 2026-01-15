298707d9f2398d0b1b5ad10bdc0b53aa346f389d
IonelaCernat <ionelacernat10@gmail.com>
2026-01-15 02:37:16 +0200

    config azure v3

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,14 +1,19 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
-  process.env.DB_USER, 
+  process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    logging: false
+    port: 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
